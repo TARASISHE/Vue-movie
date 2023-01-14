26546ef2c4fcdfb5eb97b930563e0d53c9c56bf3
Taras <taras.malchenko@gmail.com>
2023-01-14 16:38:45 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <Header v-if="routeName? headerCheck : false" /> 
+  <Home v-if="check? HomeCheck: false" />
   <RouterView />
 </template>
 
 
 <script setup>
 import Header from './components/Header.vue';
+import Home from './views/Home.vue';
 import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 
 const headerCheck = ref(true);
+const HomeCheck = ref(false);
 const router = useRouter();
 
 const routeName = computed(() => {
   return router.currentRoute.value.path !== '/login' && router.currentRoute.value.path !== '/register'; 
+});
+
+const check = computed(() => {
+  return router.currentRoute.value.path === '/login' ; 
 });
 
 </script>
