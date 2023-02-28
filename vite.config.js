e49f75cb3971bdaa2c527e9e3cdad4c186980bcb
Taras <taras.malchenko@gmail.com>
2023-02-28 18:26:44 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: 'https://tarasishe.github.io/Vue-movie/',
+  base: '/Vue-movie/',
   build: {
     chunkSizeWarningLimit: 1600
   }
